c47f7a246023b6f361498b02328b4109f77654c4
unknown <you@example.com>
2025-11-16 16:34:31 -0600

    create

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ragen-ai.github.io",
-    base: '/',
+    site: "https://WorldAgen.github.io",
+    base: '/WorldAgen/',
 });
